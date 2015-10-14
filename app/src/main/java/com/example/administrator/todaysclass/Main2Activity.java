ecce4a18d409f21f2259ebc821421ce9e24f1568
Atiar Talukdar <atiartalukdar@gmail.com>
2015-10-14 21:04:10 +0600

    string reverse

--- app/src/main/java/com/example/administrator/todaysclass/Main2Activity.java
+++ app/src/main/java/com/example/administrator/todaysclass/Main2Activity.java
@@ -1,39 +1,40 @@
 package com.example.administrator.todaysclass;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-EditText ed;
+public class Main2Activity extends AppCompatActivity {
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ed = (EditText) findViewById(R.id.editText);
-
+        setContentView(R.layout.activity_main2);
+        tv = (TextView) findViewById(R.id.tv11);
+        String a = getIntent().getStringExtra("REV").toString();
+        tv.setText(a);
     }
-    public void Reverse(View view){
-        String str = ed.getText().toString();
-        String p="";
-        for(int i=str.length()-1;i>=0;i--){
-            p += str.charAt(i);
-        }
 
-        Intent intent = new Intent(this,Main2Activity.class);
-        intent.putExtra("REV",p);
-        startActivity(intent);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
